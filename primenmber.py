308de8805801cfaf0904c35c07bbacc08548785f
Umakambampati <kambampatiumamaheshwari@gmail.com>
2026-06-09 12:02:53 +0530

    changing logic and added lcm

--- primenmber.py
+++ primenmber.py
@@ -1,10 +1,10 @@
 def prime(num):
     is_prime=True
-    if num==1 or num<=/0:
+    if num==1 or num<=0:
         return False
-    for i in range(2,num):
+    for i in range(2,int(num**0.5)+1):
         if num%i==0:
             is_prime=False
             break
     return is_prime
-print(prime(8))
+print(prime(3))
--- primenumberrange.py
+++ primenumberrange.py
@@ -4,7 +4,7 @@ def primerange(start,stop):
         if i<=1:
             continue
         is_prime=True
-        for j in range(2,i):
+        for j in range(2,int(i**0.5)+1):
             if i%j==0:
                 is_prime=False
                 break
